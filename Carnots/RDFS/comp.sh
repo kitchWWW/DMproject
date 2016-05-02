7c623867a279c0d7814ec0c4a0b0d09a0be341fc
Brian <brian.e2014@gmail.com>
2016-05-02 14:56:42 -0500

    Added RDFS
    RDFS inferencer added.

---
+++ Carnots/RDFS/comp.sh
@@ -0,0 +1,7 @@
+export INSTALLATION_ROOT="/Users/Admin/Documents/CompSci/DMproject/Carnots/RDFS/jyhton"
+cd jyhton
+ant clean
+rm -r -f dist
+ant
+ant
+cd ..
---
+++ Carnots/RDFS/run.sh
@@ -0,0 +1,3 @@
+echo "about to run Carnot!"
+export INSTALLATION_ROOT="/Users/Admin/Documents/CompSci/DMproject/Carnots/RDFS/jyhton"
+jyhton/dist/bin/jython
